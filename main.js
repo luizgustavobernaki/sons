5b83b3e79a36054951d76f3f750dffce14f12201
Luiz Gustavo Bernaki <103518504+luizgustavobernaki@users.noreply.github.com>
2023-11-09 21:57:38 -0300

    teste

--- main.js
+++ main.js
@@ -6,21 +6,16 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
 //para
-for (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
 const tecla = listaDeTeclas[contador];
 
     const instrumento = tecla.classList[1];
-    // tamplte string
-    const idAudio = `#som_${instrumento}`;
-   // console.log(idAudio);
+   
+    const idAudio = `#som_${instrumento}`; // tamplte string
     tecla.onclick = function(){
         tocaSom(idAudio)
     }
-    contador = contador + 1;
-    
-    //console.log(contador);
+    console.log(contador);
 }
